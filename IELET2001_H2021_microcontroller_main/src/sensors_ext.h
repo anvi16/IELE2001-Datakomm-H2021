593bb52db1ad6d86ef7c6605b89fb01bc0e65ea7
JorgenAndreasMo <jorgenandreasmo@gmail.com>
2021-11-04 22:39:32 +0100

    Created VS Code project
    Created VS Code project

---
+++ IELET2001_H2021_microcontroller_main/src/sensors_ext.h
@@ -0,0 +1,36 @@
+
+#ifndef SENSORS_EXT
+#define SENSORS_EXT
+
+class GPS{
+    
+    private:
+
+    public:
+
+    int getLatitude();
+    int getLongitude();
+
+};
+
+
+class GPS{
+    
+    private:
+
+
+    public:
+
+    int getLatitude();
+    int getLongitude();
+
+};
+
+
+
+
+
+
+
+
+#endif
