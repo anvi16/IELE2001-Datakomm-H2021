3241be5d9a83bc0963b2b612171fb65253442be2
Lars-Kvan <70978511+Lars-Kvan@users.noreply.github.com>
2021-11-12 17:03:53 +0100

    Update main.cpp

--- IELET2001_H2021_microcontroller_main/src/main.cpp
+++ IELET2001_H2021_microcontroller_main/src/main.cpp
@@ -1,67 +1,63 @@
-
+#include <Arduino.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include "sensors_ext.h"
-#include "config.h"
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include <Wire.h>
 
-
-
-// GPS - homemade
-
-GPS gps(GPSBaud, psGPS, txGPS, rxGPS);
-
-
+TFT_eSPI tft = TFT_eSPI(135, 240);
 
 // GPS
-TinyGPSPlus gpsx; // The TinyGPS++ object
-
-
+TinyGPSPlus gps; // The TinyGPS++ object
 
 // Software Serial 
 // Because the hardware-based serial communication pins are busy, we create new by using the SoftwareSerial library
+static const int RXPin =    21;
+static const int TXPin =    22;
+static const uint32_t GPSBaud = 9600;
+SoftwareSerial ssGPS(RXPin, TXPin);            // Create software-based serial communication on pins
 
-SoftwareSerial ssGPS(rxGPS, txGPS);            // Create software-based serial communication on pins
+const int LEDPin =          12;
 
+// Functionaly the same as the "delay()" function. D
+void espDelay(int ms)
+{
+  esp_sleep_enable_timer_wakeup(ms * 1000);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+  esp_light_sleep_start();
+}
 
 void setup() {
   
-  // Start serial communication with microcontroller
-  Serial.begin(serialBaud);
-
-  // GPS - Configure hardware for GPS sensor
-  gps.setGPSPsPin(psGPS);
-  gps.setGPSRxPin(rxGPS);
-  gps.setGPSTxPin(txGPS);
-
-  
-
-
-
-
-
-
+  Serial.begin(9600);
+  pinMode(LEDPin, OUTPUT);
 
   // Set baud rate for GPS communication
   ssGPS.begin(GPSBaud); // Serial communication with GPS module
 
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.setTextColor(TFT_GREEN);
+  tft.print("Test");
 }
-
-
-
 
 
 
 
 void displayInfo()
 {
-  if (gpsx.location.isValid())
+  if (gps.location.isValid())
   {
     Serial.print("Latitude: ");
-    Serial.println(gpsx.location.lat(), 6);
+    Serial.println(gps.location.lat(), 6);
     Serial.print("Longitude: ");
-    Serial.println(gpsx.location.lng(), 6);
+    Serial.println(gps.location.lng(), 6);
     Serial.print("Altitude: ");
-    Serial.println(gpsx.altitude.meters());
+    Serial.println(gps.altitude.meters());
   }
   else
   {
@@ -69,13 +65,13 @@ void displayInfo()
   }
   
   Serial.print("Date: ");
-  if (gpsx.date.isValid())
+  if (gps.date.isValid())
   {
-    Serial.print(gpsx.date.month());
+    Serial.print(gps.date.month());
     Serial.print("/");
-    Serial.print(gpsx.date.day());
+    Serial.print(gps.date.day());
     Serial.print("/");
-    Serial.println(gpsx.date.year());
+    Serial.println(gps.date.year());
   }
   else
   {
@@ -83,19 +79,19 @@ void displayInfo()
   }
 
   Serial.print("Time: ");
-  if (gpsx.time.isValid())
+  if (gps.time.isValid())
   {
-    if (gpsx.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gpsx.time.hour());
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
     Serial.print(":");
-    if (gpsx.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gpsx.time.minute());
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
     Serial.print(":");
-    if (gpsx.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gpsx.time.second());
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
     Serial.print(".");
-    if (gpsx.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.println(gpsx.time.centisecond());
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.println(gps.time.centisecond());
   }
   else
   {
@@ -113,7 +109,7 @@ void loop()
 {
   // This sketch displays information every time a new sentence is correctly encoded.
   while (ssGPS.available() > 0){
-    if (gpsx.encode(ssGPS.read())){
+    if (gps.encode(ssGPS.read())){
       displayInfo();
       byte gpsData = ssGPS.read();
       Serial.println(gpsData);
@@ -121,7 +117,7 @@ void loop()
   }
   // If 5000 milliseconds pass and there are no characters coming in
   // over the software serial port, show a "No GPS detected" error
-  if (millis() > 5000 && gpsx.charsProcessed() < 10)
+  if (millis() > 5000 && gps.charsProcessed() < 10)
   {
     Serial.println("No GPS detected");
     while(true);
