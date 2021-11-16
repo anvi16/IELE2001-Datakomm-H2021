ba361d00c594d2f1723266e7866c0ad641a91f4e
JorgenAndreasMo <jorgenandreasmo@gmail.com>
2021-11-16 11:45:29 +0100

    Stash added

--- IELET2001_H2021_microcontroller_main/src/config.h
+++ IELET2001_H2021_microcontroller_main/src/config.h
@@ -12,16 +12,14 @@
 //////////////////////////////////////////////////////
 
 // I2C interface pins
-const int i2cSDA =    21;
-const int i2cSCL =    22;
+const int i2cSDA =          21;   // Green wire
+const int i2cSCL =          22;   // Yellow wire
+const int i2cBME280Adr =    0x76; // BME280 adress
 
 // GPS
-const int psGPS =    25;     // Power supply pin for GPS sensor ---------- SELECT OTHER PIN !
-const int rxGPS =    37;     // Serial recieve pin GPS
-const int txGPS =    38;     // Serial transmit pin GPS
-
-
-
+const int psGPS =    25;          // Power supply pin for GPS sensor ---------- SELECT OTHER PIN !
+const int rxGPS =    37;          // Serial recieve pin GPS
+const int txGPS =    38;          // Serial transmit pin GPS
 
 
 //////////////////////////////////////////////////////
@@ -33,3 +31,34 @@ const int txGPS =    38;     // Serial transmit pin GPS
 // GPS
 static const uint32_t serialBaud = 9600;
 static const uint32_t GPSBaud = 9600;
+
+
+
+
+
+//////////////////////////////////////////////////////
+//                                                  //
+//                     UBIDOTS                      //
+//                                                  //
+//////////////////////////////////////////////////////
+
+// Based on example https://help.ubidots.com/en/articles/748067-connect-an-esp32-devkitc-to-ubidots-over-mqtt
+
+// Library dependencies
+
+#include <UbidotsEsp32Mqtt.h>
+
+/****************************************
+ * Define Constants
+ ****************************************/
+const char *UBIDOTS_TOKEN = "BBFF-pzGZkRPVMO7baDRylwiST623Ow5DrF";  // Put here your Ubidots TOKEN
+const char *WIFI_SSID = "Inteno-D81D";      // Put here your Wi-Fi SSID
+const char *WIFI_PASS = "XJRBC6IITC42CO";   // Put here your Wi-Fi password
+const char *DEVICE_LABEL = "Aalesund";       // Put here your Device label to which data will be published
+const char *VARIABLE_LABEL = "Temperature";        // Put here your Variable label to which data  will be published
+
+const int PUBLISH_FREQUENCY = 5000;         // Update rate in milliseconds
+
+unsigned long timer;
+
+
--- IELET2001_H2021_microcontroller_main/src/main.cpp
+++ IELET2001_H2021_microcontroller_main/src/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <Arduino.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
@@ -19,6 +20,45 @@ static const uint32_t GPSBaud = 9600;
 SoftwareSerial ssGPS(RXPin, TXPin);            // Create software-based serial communication on pins
 
 const int LEDPin =          12;
+=======
+
+// Include libraries
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>                     // Software serial library
+#include <time.h>                               // Time library Arduino
+
+#include <cstdio>
+#include <iostream>
+
+// Include files with dependencies
+#include "sensors_ext.h"                        // File containging classes related to peripheral sensors (GPS, temp, hum, press)
+#include "config.h"                             // Config file for HW pins, baud etc.
+
+// GPS class - Constructor takes GPS module baud rate and pins for PS, Tx and Rx
+GPS gps(GPSBaud, serialBaud, psGPS, txGPS, rxGPS);
+WeatherStation ws(i2cBME280Adr);
+
+Ubidots ubidots(UBIDOTS_TOKEN);
+
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+
+>>>>>>> Stashed changes
 
 // Functionaly the same as the "delay()" function. D
 void espDelay(int ms)
@@ -28,14 +68,38 @@ void espDelay(int ms)
   esp_light_sleep_start();
 }
 
+// Setup
 void setup() {
   
+<<<<<<< Updated upstream
   Serial.begin(9600);
   pinMode(LEDPin, OUTPUT);
+=======
+  // Start serial communication with microcontroller
+  Serial.begin(serialBaud);                     
+  Serial.println("Booting up");      
 
-  // Set baud rate for GPS communication
-  ssGPS.begin(GPSBaud); // Serial communication with GPS module
+  // ubidots.setDebug(true);  // uncomment this to make debug messages available
+  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
+  ubidots.setCallback(callback);
+  ubidots.setup();
+  ubidots.reconnect();
 
+  timer = millis();           
+
+  // Enable external sensors
+  gps.enable();                                 // Power up GPS module and establish serial com 
+  ws.enable();                                  // Establish I2C com with Weather Station
+
+}
+
+
+void loop(){
+>>>>>>> Stashed changes
+
+  gps.refresh();                                // Update data from GPS
+
+<<<<<<< Updated upstream
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_WHITE);
@@ -44,10 +108,31 @@ void setup() {
   tft.setTextColor(TFT_GREEN);
   tft.print("Test");
 }
+=======
+  // Ubidots
+  if (!ubidots.connected())
+  {
+    ubidots.reconnect();
+  }
+>>>>>>> Stashed changes
 
+  if (abs(millis() - timer) > PUBLISH_FREQUENCY) // triggers the routine every 5 seconds
+  {
+    char gpsData[1000] = "";
+    
+    // Format latitude and longitude in a string
+    sprintf(gpsData, "\"lat\":%.6f, \"lng\":%.6f", gps.getLatitude(), gps.getLongitude());
 
+    // Ubidots publish
+    //          "Variable"        "Value"         "Context"
+    ubidots.add("gps",            1,              gpsData   );  // Generate / update GPS variable in Ubidots 
+    ubidots.add("Temperature",    ws.getTempC()             );  // Generate / update Temperature variable in Ubidots
+    ubidots.add("Humidity",       ws.getHum()               );  // Generate / update Temperature variable in Ubidots
+    ubidots.add("Pressure [hPa]", ws.getPressHPa()          );  // Generate / update Temperature variable in Ubidots
 
+    ubidots.publish(DEVICE_LABEL);              // Publish buffer to Ubidots
 
+<<<<<<< Updated upstream
 void displayInfo()
 {
   if (gps.location.isValid())
@@ -97,14 +182,59 @@ void displayInfo()
   {
     Serial.println("Not Available");
   }
+=======
+    Serial.println(gpsData);                    // Print context (debug)
+
+    timer = millis();
+  }
+
+  ubidots.loop();
+
+  // Print position
+  Serial.print("Current position:  ");
+  Serial.print(gps.getLatitude(),6);
+  Serial.print("N, ");
+  Serial.print(gps.getLongitude(),6);
+  Serial.println("E");
+  
+  // Print date
+  Serial.print("Current date:      ");
+  Serial.print(gps.getDay());
+  Serial.print("/");
+  Serial.print(gps.getMonth());
+  Serial.print("-");
+  Serial.println(gps.getYear());
+
+  // Print time
+  Serial.print("Current time:      ");
+  Serial.print(gps.getHour());
+  Serial.print(":");
+  Serial.print(gps.getMinute());
+  Serial.print(":");
+  Serial.println(gps.getSecond());
+  Serial.print("Time age:");
+  Serial.println(gps.getTimeAge());
+>>>>>>> Stashed changes
 
   Serial.println();
+
+  // Print weather
+  Serial.print("Current temp:      ");
+  Serial.println(ws.getTempC());
+  Serial.print("Current press:     ");
+  Serial.println(ws.getPressHPa());
+  Serial.print("Current hum:       ");
+  Serial.println(ws.getHum());
+
   Serial.println();
-  delay(1000);
+
+  delay(0);
+
 }
 
 
 
+<<<<<<< Updated upstream
 void loop()
 {
   // This sketch displays information every time a new sentence is correctly encoded.
@@ -122,41 +252,33 @@ void loop()
     Serial.println("No GPS detected");
     while(true);
   }
+=======
+>>>>>>> Stashed changes
 
-  
-}
 
 
-/*
-void loop(){
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (ssGPS.available() > 0){
-    gps.encode(ssGPS.read());
-    if (gps.location.isUpdated()){
-      Serial.print("Latitude= "); 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(gps.location.lng(), 6);
-    }
-  }
-}
-*/
 
-  /*
-  // Displays information when new sentence is available.
-  while (ssGPS.available() > 0){
-    Serial.write(ssGPS.read());
-  }
-    
-  if (gps.location.isValid()) {
-    double latitude = (gps.location.lat());
-    double longitude = (gps.location.lng());
-    Serial.println("Lat");
-    Serial.println(latitude);
-    Serial.println("Long");
-    Serial.println(longitude);
-  }
-  */
 
-  // GPS displayInfo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
